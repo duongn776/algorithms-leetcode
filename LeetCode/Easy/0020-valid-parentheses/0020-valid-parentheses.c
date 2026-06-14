8ebf39b1a86d7452a29cb3119c8303e54f3d5dfe
Nguyen Hoang Duong <nhduong776@gmail.com>
2026-06-14 15:02:48 +0700

    0020-valid-parentheses

---
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.c
@@ -0,0 +1,30 @@
+bool isValid(char* s) {
+    int stack[10000];
+    int top = -1;
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        char c = s[i];
+
+        if ((c == '(') || (c == '[') || (c == '{'))
+        {
+            stack[++top] = c;
+        }
+        else
+        {
+            if (top == -1)
+            {
+                return false;
+            }
+            else
+            {
+                char t = stack[top--];
+                if ((c == ')' && t != '(') || (c == ']' && t != '[') || (c == '}' && t != '{'))
+                {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return top == -1;
+}
